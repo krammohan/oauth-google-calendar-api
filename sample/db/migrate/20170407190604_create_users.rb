6344bebf364339584bcc32d3b4b025ae25b4fe14
krammohan <kavya.ramm@gmail.com>
2017-04-07 14:58:47 -0700

    Adds functionality to log in with google account

--- sample/db/migrate/20170407190604_create_users.rb
+++ sample/db/migrate/20170407190604_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :username
+      t.string :provider
+      t.string :uid
+      t.string :email
+      t.string :password
+      t.string :token
       t.string :access_token
       t.string :refresh_token
       t.timestamps
